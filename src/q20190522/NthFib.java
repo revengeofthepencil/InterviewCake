aac9589964bb45a2171a18ef94fd0beba4b9c543
awalker <revengeofthepencil@gmail.com>
2019-05-29 09:48:58 -0700

    add memoize to nth fib

--- src/q20190522/NthFib.java
+++ src/q20190522/NthFib.java
@@ -1,9 +1,14 @@
 package q20190522;
 
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
 
 public class NthFib {
+    private Map<Integer, Integer> memoizeFib = new HashMap<Integer, Integer>();
+    private int memoizeHits = 0;
+
     public int getNthFib(int fibIdx) {
         if (fibIdx <= 1) {
             return fibIdx;
@@ -12,13 +17,49 @@ public class NthFib {
         }
     }
 
+    public int getNthFibMemoize(int fibIdx) {
+        if (fibIdx <= 1) {
+            return fibIdx;
+        } else if (memoizeFib.containsKey( fibIdx )) {
+            memoizeHits += 1;
+            return memoizeFib.get(fibIdx);
+        } else {
+            int fib = getNthFibMemoize(fibIdx - 1) + getNthFibMemoize(fibIdx - 2);
+            memoizeFib.put(fibIdx, fib);
+            return fib;
+        }
+    }
+
+    public int getMemoizeHits() {
+        return memoizeHits;
+    }
+
+
+    public Map<Integer, Integer> getMemoizeFib() {
+        return memoizeFib;
+    }
+
     public static void main(String[] args) {
         Random rand = new Random();
-        final int fibIdx = rand.nextInt(30);
+        final int fibIdx = rand.nextInt(50);
         NthFib nf = new NthFib();
+
+
         for (int i = 0; i <= fibIdx; i++) {
+            long ms = System.currentTimeMillis();
             int nthFib = nf.getNthFib(i);
-            System.out.println("fibIdx = " + fibIdx + ", i = " + i + ", nthFib = " + nthFib);
+            long runtime = System.currentTimeMillis() - ms;
+            System.out.println("getNthFib, runtime = " + runtime + ", fibIdx = " + fibIdx + ", i = " + i + ", nthFib = " + nthFib);
         }
+
+        for (int i = 0; i <= fibIdx; i++) {
+            long ms = System.currentTimeMillis();
+            int nthFib = nf.getNthFibMemoize(i);
+            long runtime = System.currentTimeMillis() - ms;
+            int memoizeHits = nf.getMemoizeHits();
+            System.out.println("getNthFibMemoize, memoizeHits = " + memoizeHits + ", runtime = " + runtime + ", fibIdx = " + fibIdx + ", i = " + i + ", nthFib = " + nthFib);
+        }
+
+
     }
 }
